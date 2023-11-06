a65d96609064cefb46e7f8f060039fa917acbb9d
hyeri00 <nexusring@gmail.com>
2023-11-06 17:35:28 +0900

    feat: Set up navigation bar in GeneralBoardViewController

--- mailplug-assignment/Presentation/GeneralBoardViewController.swift
+++ mailplug-assignment/Presentation/GeneralBoardViewController.swift
@@ -21,6 +21,39 @@ class GeneralBoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setNavigationBar()
+    }
     
+    private func setNavigationBar() {
+        let leftImage = IconImage.menuBar.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        let rightImage = IconImage.searchBar.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        
+        navigationController?.navigationBar.barTintColor = .white
+        
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.topItem?.title = "일반 게시판"
+            navigationBar.titleTextAttributes = [
+                .font: UIFont.systemFont(ofSize: 16),
+                .foregroundColor: UIColor.black
+            ]
+        }
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: leftImage, style: .done,
+            target: self, action: #selector(showMenuBar))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: rightImage, style: .done,
+            target: self, action: #selector(showSearchBar))
+    }
+    
+    @objc
+    private func showMenuBar() {
+        // TODO - menuBar 보여주기
+    }
+    
+    @objc
+    private func showSearchBar() {
+        // TODO - searchBar 보여주기
     }
 }
