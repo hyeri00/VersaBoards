85150a80aea2d4a42d29f2afac2871dfde78a980
hyeri00 <nexusring@gmail.com>
2023-11-07 22:03:16 +0900

    base configure

--- mailplug-assignment/Presentation/Search/Views/SearchResultTableViewCell.swift
+++ mailplug-assignment/Presentation/Search/Views/SearchResultTableViewCell.swift
@@ -18,4 +18,23 @@ class SearchResultTableViewCell: UITableViewCell {
         label.font = Font.Typography.regular14
         return label
     }()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+        self.selectionStyle = .none
+    }
 }
