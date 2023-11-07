374c987963e047abbb24d5a539d843d16bef9740
hyeri00 <nexusring@gmail.com>
2023-11-07 21:54:08 +0900

    configure default searchBar

---
+++ mailplug-assignment/DesignSystem/Components/SearchBar.swift
@@ -0,0 +1,26 @@
+//
+//  SearchBar.swift
+//  mailplug-assignment
+//
+//  Created by 혜리 on 2023/11/07.
+//
+
+import UIKit
+
+class SearchBar: UISearchBar {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureDefault()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func configureDefault() {
+        self.layer.cornerRadius = 4
+        self.searchBarStyle = .minimal
+    }
+}
