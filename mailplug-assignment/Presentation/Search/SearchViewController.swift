b52eab1faf7eb3a4175f27400ac240fc39b3ce08
hyeri00 <nexusring@gmail.com>
2023-11-07 22:11:37 +0900

    snapkit 활용하여 layout 구성 및 Enum Metric으로 일관된 간격과 크기 조정

--- mailplug-assignment/Presentation/Search/SearchViewController.swift
+++ mailplug-assignment/Presentation/Search/SearchViewController.swift
@@ -10,6 +10,13 @@ import SnapKit
 
 class SearchViewController: UIViewController {
     
+    // MARK: - Metric
+    
+    enum Metric {
+        static let topMargin: CGFloat = 10
+        static let searchBarLeftRightMargin: CGFloat = 15
+    }
+
     // MARK: - UI
     
     private let searchBar: SearchBar = {
@@ -38,5 +45,20 @@ class SearchViewController: UIViewController {
     
     private func configure() {
         view.backgroundColor = Colors.grey_100
+    }
+    
+    private func makeConstraints() {
+        self.view.addSubview(self.searchBar)
+        self.view.addSubview(self.resultTableView)
+        
+        self.searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(Metric.topMargin)
+            $0.leading.trailing.equalToSuperview().inset(Metric.searchBarLeftRightMargin)
+        }
+        
+        self.resultTableView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(Metric.topMargin)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
