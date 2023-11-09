51d4a020b00288b427ad9922d5ae0ed32bd5aa63
hyeri00 <nexusring@gmail.com>
2023-11-09 17:04:54 +0900

    구조 변경위한 DTO 생성

---
+++ mailplug-assignment/Repository/DTO/SearchTextDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultDTO.swift
+//  mailplug-assignment
+//
+//  Created by 혜리 on 11/9/23.
+//
+
+import Foundation
+
+struct SearchResultDTO {
+    let title: String
+    let keyword: String
+}
